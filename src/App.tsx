7cac222ce59cd0e34c97d7cbf67530cddcf9422c
DiegoCard117 <diegocardoso117@gmail.com>
2023-09-14 01:15:47 -0300

    conversao de video>audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
+import { Github, Wand2 } from "lucide-react";
 import { Label } from "@radix-ui/react-label";
 
 import { Textarea } from "./components/ui/textarea";
@@ -6,6 +6,7 @@ import { Separator } from "./components/ui/separator";
 import { Button } from "./components/ui/button";
 import { Select, SelectItem, SelectValue, SelectTrigger, SelectContent } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
 
 export function App() {
 
@@ -38,34 +39,7 @@ export function App() {
           <p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variavel <code className="text-red-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado</p>
         </div>
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um video
-            </label>
-            <input type="file" name="video" id="video" accept="video/mp4" className="sr-only" />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label
-                htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras chaves mencionadas no video separadas por vírgula ( , )"
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Carregar Video
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm/>
           <Separator />
           <form className="space-y-6">
             <div className="space-y-2">
@@ -92,10 +66,8 @@ export function App() {
                   <SelectItem value="gpt3.5">GPT 3.5-Turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-            </div>
-
+            </div> 
             <Separator />
-
             <div className="space-y-4">
               <Label>Temperatura</Label>
               <Slider
